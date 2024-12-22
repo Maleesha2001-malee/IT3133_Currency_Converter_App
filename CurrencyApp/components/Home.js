33f1b3a483663b96401761cd2a88207258973060
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-22 08:34:23 +0000

    add value and Change

--- CurrencyApp/components/Home.js
+++ CurrencyApp/components/Home.js
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
-import CurrencyInput from './components/CurrencyInput';
+import CurrencyInput from './CurrencyInput';
 
 export default function Home() {
-  
+    const [amountLKR, setAmountLKR] = useState('');
 
   return (
-    <View style={styles.container}>
+    <>
       <Text variant="headlineLarge">LKR to USD Converter</Text>
       <CurrencyInput value={amountLKR} onChange={setAmountLKR} />
       
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
