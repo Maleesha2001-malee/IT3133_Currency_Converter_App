1beb8d533b39304dedbd3668268859298369fda0
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-22 08:14:55 +0000

    import Currency Input file to Home

--- Components/Home.js
+++ Components/Home.js
@@ -1,3 +1,9 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
+import CurrencyInput from './components/CurrencyInput';
+
+
 export default function Home() {
     return();
 }
