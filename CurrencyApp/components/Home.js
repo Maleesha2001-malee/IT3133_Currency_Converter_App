bb946b4a48374bbcfcb63a0e573de4ae6db8885d
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-22 08:40:17 +0000

    add Convert Button and Convert Currency function

--- CurrencyApp/components/Home.js
+++ CurrencyApp/components/Home.js
@@ -2,14 +2,28 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 import CurrencyInput from './CurrencyInput';
+import ConvertButton from './ConvertButton';
 
 export default function Home() {
     const [amountLKR, setAmountLKR] = useState('');
+    const [convertedUSD, setConvertedUSD] = useState(null);
+    const exchangeRate = 300;
+  
+    const convertCurrency = () => {
+      const lkr = parseFloat(amountLKR);
+      if (!isNaN(lkr)) {
+        setConvertedUSD((lkr * exchangeRate).toFixed(2));
+      } else {
+        setConvertedUSD(null);
+        alert('Please enter a valid amount.');
+      }
+    };
 
   return (
     <>
       <Text variant="headlineLarge">LKR to USD Converter</Text>
       <CurrencyInput value={amountLKR} onChange={setAmountLKR} />
+      <ConvertButton onPress={convertCurrency} />
       
     </>
   );
