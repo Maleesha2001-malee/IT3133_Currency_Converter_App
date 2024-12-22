fd1b2bd3f8bf82e118d3a65fde0f51f1c06574d0
Maleesha Rathnayake <139684247+Maleesha2001-malee@users.noreply.github.com>
2024-12-22 08:36:14 +0000

    add style to home

--- CurrencyApp/components/Home.js
+++ CurrencyApp/components/Home.js
@@ -7,12 +7,20 @@ export default function Home() {
     const [amountLKR, setAmountLKR] = useState('');
 
   return (
-    <>
+    <View style={styles.container}>
       <Text variant="headlineLarge">LKR to USD Converter</Text>
       <CurrencyInput value={amountLKR} onChange={setAmountLKR} />
       
-    </>
+    </View>
   );
 }
-
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
 
